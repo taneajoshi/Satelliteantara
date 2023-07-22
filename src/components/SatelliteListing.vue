86f51b6ba45af64d72001983fc149b4a01dd6ccf
“taneajoshi” <taneajoshi@gmail.com>
2023-07-22 22:46:13 +0530

    WIP: added filters (without debounce)

--- src/components/SatelliteListing.vue
+++ src/components/SatelliteListing.vue
@@ -1,6 +1,102 @@
 <template>
   <section>
+    <!-- Filters -->
+    <div class="filters mb-4 p-1">
+      <form
+        @keydown.enter.prevent="getFilteredSatellites"
+        v-form-validity="filterForm"
+      >
+        <!-- Search -->
+        <ErrorComponent :form-control="filterForm.get('search')">
+          <div class="input-group position-relative">
+            <input
+              class="form-control rounded-3"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+              v-model="filterForm.get('search').value"
+              v-input-validity="filterForm.get('search')"
+              @input="getFilteredSatellites"
+            />
+            <span
+              class="input-group-text position-absolute text-secondary translate-middle-y top-50"
+            >
+              <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+            </span>
+          </div>
+        </ErrorComponent>
+        <!-- /Search -->
+
+        <div class="d-flex gap-3 flex-wrap mt-4">
+          <!-- Country Code -->
+          <div class="form-group flex-grow-1">
+            <ErrorComponent :form-control="filterForm.get('countryCode')">
+              <select
+                class="form-select"
+                v-model="filterForm.get('countryCode').value"
+                @change="getFilteredSatellites"
+              >
+                <option value="">Country</option>
+                <option
+                  v-for="country in countries"
+                  :key="country.code"
+                  :value="country.code"
+                >
+                  {{ country.name }}
+                </option>
+              </select>
+            </ErrorComponent>
+          </div>
+          <!-- /Country Code -->
+
+          <!-- Orbit -->
+          <div class="form-group flex-grow-1">
+            <ErrorComponent :form-control="filterForm.get('orbitCode')">
+              <select
+                class="form-select"
+                v-model="filterForm.get('orbitCode').value"
+                @change="getFilteredSatellites"
+              >
+                <option value="">Orbit</option>
+                <option
+                  v-for="orbit in orbits"
+                  :key="orbit.code"
+                  :value="orbit.code"
+                >
+                  {{ orbit.name }}
+                </option>
+              </select>
+            </ErrorComponent>
+          </div>
+          <!-- /Orbit -->
+
+          <!-- Object Type-->
+          <div class="form-group flex-grow-1">
+            <ErrorComponent :form-control="filterForm.get('objectType')">
+              <select
+                class="form-select"
+                v-model="filterForm.get('objectType').value"
+                @change="getFilteredSatellites"
+              >
+                <option value="">ObjectType</option>
+                <option
+                  v-for="type in objectTypes"
+                  :key="type.code"
+                  :value="type.code"
+                >
+                  {{ type.name }}
+                </option>
+              </select>
+            </ErrorComponent>
+          </div>
+          <!-- /Object Type -->
+        </div>
+      </form>
+    </div>
+    <!-- /Filters -->
+
     <div class="mb-4 h4"><small>Took 1.2 ms</small></div>
+
     <div class="d-flex flex-wrap">
       <div
         class="satellite-card d-flex gap-4 align-items-center mb-5"
@@ -26,27 +122,149 @@
 </template>
 
 <script setup lang="ts">
+import { finalize, switchMap } from "rxjs";
 import { onMounted, ref, watch } from "vue";
+import { applicationContainer } from "../container/container";
 import { SatelliteInterface } from "../interfaces/SatelliteInterface";
+import { LoadingService } from "../services/loading.service";
+import { SatelliteService } from "../services/satellite.service";
+import { ToastService } from "../services/toast.service";
 import { SatelliteImages } from "../assets/data/satellitesImage.json";
+import { useRoute, useRouter } from "vue-router";
+import { FormControl, FormGroup } from "universal-reactive-forms";
+import { noWhiteSpaceValidation } from "../utils/validations";
+import { PaginationQuery } from "../interfaces/paginatedInterface";
+import { FilterInterface } from "../interfaces/filterInterface";
+import ErrorComponent from "./ErrorComponent.vue";
+import { InputValidity as vInputValidity } from "../directives/input-validity";
+import { FormValidity as vFormValidity } from "../directives/form-validity";
+
+// Dependency Injections
+const satelliteService = applicationContainer.resolve(SatelliteService);
+const loadingService = applicationContainer.resolve(LoadingService);
+const toastService = applicationContainer.resolve(ToastService);
 
 const satelliteData = ref<SatelliteInterface[] | null>([]);
 const shuffledImageUrls = ref<string[]>([]);
+const route = useRoute();
+const router = useRouter();
+const filterOptions = ref<Filter>({});
+const searchParams = new URLSearchParams(route.query as any);
+
+interface Filter {
+  [key: string]: string | undefined;
+}
 
 onMounted(() => {
-  shuffleImages();
+  loadSatellitesData();
+  initFiltersFromRouteParams(route.query);
 });
 
-const props = defineProps<{
-  filteredData: any;
-}>();
+/**
+ * Filter Form
+ **/
+const filterForm = new FormGroup({
+  search: new FormControl("", [noWhiteSpaceValidation]),
+  countryCode: new FormControl(""),
+  orbitCode: new FormControl(""),
+  objectType: new FormControl(""),
+});
 
+// Define the lists of countries, orbits, and object types here.
+const countries = ref([
+  { code: "US", name: "United States of America" },
+  { code: "PRC", name: "People's Republic of China" },
+  { code: "INDO", name: "Indonesia" },
+  { code: "TBD", name: "To be decided" },
+]);
+
+const orbits = ref([
+  { code: "LEO", name: "Leo" },
+  { code: "HEO", name: "Heo" },
+  { code: "CIS", name: "Commonwealth of Independent States " },
+  { code: "GEO", name: "Geo" },
+]);
+
+const objectTypes = ref([
+  { code: "DEBRIS", name: "Debris" },
+  { code: "PAYLOAD", name: "Payload" },
+]);
+
+/**
+ * Shuffle dummy images to use as satellite image.
+ **/
 function shuffleImages() {
   shuffledImageUrls.value = SatelliteImages.sort(() => 0.5 - Math.random());
 }
 
-watch(props, () => {
-  satelliteData.value = props.filteredData;
+/**
+ * Initialize the filters depending on query params on url
+ **/
+function initFiltersFromRouteParams(query: any) {
+  const filterValues = filterForm.getRawValue();
+
+  Object.entries(filterValues).forEach(([key]) => {
+    filterOptions.value[key] = query[key] ? query[key]?.toString() : "";
+    filterForm.get(key.toString()).value = query[key] || "";
+  });
+}
+
+/**
+ * Load Filtered and paginated Satellite data
+ **/
+function loadSatellitesData(
+  pagination: PaginationQuery = { page: 1, pageSize: 15 }
+) {
+  const paginationOption: Required<PaginationQuery> = {
+    page: pagination.page || 1,
+    pageSize: pagination.pageSize || 15,
+  };
+
+  loadingService
+    .show()
+    .pipe(
+      switchMap(() =>
+        satelliteService.listPaginated(
+          paginationOption,
+          filterOptions.value as FilterInterface
+        )
+      )
+    )
+    .pipe(finalize(() => loadingService.hide().subscribe()))
+    .subscribe({
+      next: (result) => {
+        shuffleImages();
+        satelliteData.value = result;
+      },
+      error: () => {
+        toastService.danger("Unable to load satellites.").subscribe();
+      },
+    });
+}
+
+/**
+ * Fetch filtered jobs.
+ **/
+function getFilteredSatellites() {
+  const filterValues = filterForm.getRawValue();
+  console.log("get filtered", filterValues);
+  const currentPath = route.path;
+
+  Object.entries(filterValues).forEach(([control, value]) => {
+    searchParams.set(control, value);
+    filterOptions.value[control] = value;
+  });
+
+  router.push(`${currentPath}?${searchParams.toString()}`);
+}
+
+watch(route, () => loadSatellitesData(route.query), {
+  immediate: true,
+  deep: true,
+});
+watch(route, () => initFiltersFromRouteParams(route.query), {
+  immediate: true,
+  deep: true,
 });
 </script>
 
--- src/services/satellite.service.ts
+++ src/services/satellite.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable } from "../container/container-decorator";
 import { SatelliteInterface } from "../interfaces/SatelliteInterface";
 import { HttpService } from "./http.service";
 import { PaginationQuery } from "../interfaces/paginatedInterface";
+import { FilterInterface } from "../interfaces/filterInterface";
 
 @Injectable()
 export class SatelliteService {
@@ -15,7 +16,11 @@ export class SatelliteService {
    * @returns paginated list of satellites depending on page size.
    **/
   public listPaginated(
-    pagination: Required<PaginationQuery>
+    pagination: Required<PaginationQuery> = {
+      page: 1,
+      pageSize: 15,
+    },
+    filterOption: FilterInterface = {}
   ): Observable<SatelliteInterface[]> {
     // Calculate the start index based on the page number and page size
     const startIndex = (pagination.page - 1) * pagination.pageSize;
@@ -23,6 +28,7 @@ export class SatelliteService {
       .get<SatelliteInterface[]>("/src/assets/data/satellites.json")
       .pipe(
         map((result) => {
+          console.log(filterOption);
           return result.response.slice(
             startIndex,
             startIndex + pagination.pageSize
